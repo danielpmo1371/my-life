36a761b2ef9a47e36b281605c70e992a514914e2
Daniel Paiva <danielpmo@gmail.com>
2021-09-25 18:09:15 +1200

    MADE SAVE IDS TO LOCALSTORE AND UPDATE REALTIME WORK

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import { DS } from "./DS";
-import styled from "styled-components";
 import { useCallback, useState, useRef, useEffect } from "react";
+import styled from "styled-components";
+// import { v4 as uuidv4 } from "uuid";
+import { DS } from "./DS";
 
 const EditableField = ({
+  fieldKey,
   value,
   description,
+  updateValue,
 }: {
-  value: number;
+  fieldKey: string;
+  value: string;
   description: string;
+  updateValue: (value: any, key: any) => void;
 }) => {
-  const node = useRef(); //https://medium.com/@pitipatdop/little-neat-trick-to-capture-click-outside-with-react-hook-ba77c37c7e82
+  // if (description === "uniqueId") {
+  //   console.log(`EditableField: re-rendering ${description}`);
+  //   console.log(`EditableField-key: ${fieldKey}`);
+  //   console.log(`EditableField-value: ${value}`);
+  // }
+  const node = useRef<any>();
 
   const [showInput, showInputUpdate] = useState(false);
 
-  const handleClick = useCallback(() => {
-    showInputUpdate(!showInput);
-  }, [showInput]);
+  const handleClick = useCallback((e) => {
+    if (node.current && node.current.contains(e.target)) {
+      showInputUpdate(true);
+    } else if (node.current) showInputUpdate(false);
+  }, []);
 
   const handleEnter = useCallback(() => {
+    console.log(node.current);
+    updateValue(node.current.value, fieldKey);
     showInputUpdate(!showInput);
-  }, [showInput]);
+  }, [fieldKey, showInput, updateValue]);
 
   const handleKeys = useCallback(
     (e) => {
@@ -29,60 +43,223 @@ const EditableField = ({
   );
 
   useEffect(() => {
-    // add when mounted
-    document.addEventListener("keydown", handleKeys);
-    // return function to be called when unmounted
+    if (showInput) document.addEventListener("keydown", handleKeys);
+    document.addEventListener("mousedown", handleClick);
+
     return () => {
       document.removeEventListener("keydown", handleKeys);
+      document.removeEventListener("mousedown", handleClick);
     };
-  }, [handleKeys]);
+  }, [handleClick, handleKeys, showInput]);
 
   return showInput ? (
-    <input type="text" value={value} placeholder={description} />
+    <input
+      type="text"
+      defaultValue={value}
+      placeholder={description}
+      ref={node}
+    />
   ) : (
-    <li onClick={handleClick}>(y)${value}.00</li>
+    <div
+      style={{
+        margin: "2px 20px",
+        border: "solid",
+        borderColor: "green",
+        flex: 1,
+        borderRadius: "9px",
+      }}
+      onClick={handleClick}
+      ref={node}
+    >
+      {value}
+    </div>
   );
 };
 
-const Entries = ({ value }: { value: Expense[] }) => {
+const EditableExpense = ({ yearlyValue }: { yearlyValue: number }) => {
+  return <div style={{ display: "flex" }}></div>;
+};
+
+const Entries = ({
+  entries,
+  updateEntries,
+}: {
+  entries: Expense[];
+  updateEntries: React.Dispatch<any>;
+}) => {
+  const updateUniqueId = useCallback(
+    (value, key) => {
+      console.log(`value: ${value}`);
+      console.log(`key: ${key}`);
+      console.log(entries);
+
+      const entryIndex = entries.findIndex((exp) => {
+        console.log(exp);
+
+        return exp.yearlyAmount.toString() === key;
+      });
+
+      console.log(entryIndex);
+      console.log(entries[entryIndex]);
+
+      if (entries[entryIndex]) {
+        console.log(`entry expId: ${entries[entryIndex].expenseId}`);
+
+        entries[entryIndex].expenseId = value;
+
+        console.log(`entry expId: ${entries[entryIndex].expenseId}`);
+
+        console.log(entries);
+        localStorage.setItem("budgetExpenses", JSON.stringify(entries));
+
+        updateEntries(entries);
+        console.log(entries);
+      }
+    },
+    [entries, updateEntries]
+  );
+
+  const updateExpenseValue = useCallback(
+    (value, key) => {
+      console.log(value);
+      console.log(key);
+      console.log(entries);
+
+      const entryIndex = entries.findIndex((exp) => {
+        console.log(exp);
+
+        return exp.expenseId === key;
+      });
+
+      console.log(entryIndex);
+
+      entries[entryIndex].yearlyAmount = value;
+
+      updateEntries(entries);
+    },
+    [entries, updateEntries]
+  );
+
+  const updateMonthlyExpenseValue = useCallback(
+    (value, key) => updateExpenseValue(value * 12, key),
+    [updateExpenseValue]
+  );
+
+  const updateFortnightlyExpenseValue = useCallback(
+    (value, key) => updateExpenseValue(value * 26, key),
+    [updateExpenseValue]
+  );
+
+  const updateWeeklyExpenseValue = useCallback(
+    (value, key) => updateExpenseValue(value * 52, key),
+    [updateExpenseValue]
+  );
+
+  const updateExpenseDescription = useCallback(
+    (value, key) => {
+      console.log(value);
+      console.log(key);
+      console.log(entries);
+
+      const entryIndex = entries.findIndex((exp) => {
+        console.log(exp);
+
+        return exp.expenseId === key;
+      });
+
+      console.log(entryIndex);
+
+      entries[entryIndex].description = value;
+
+      updateEntries(entries);
+    },
+    [entries, updateEntries]
+  );
+
   return (
-    <ul>
-      {value.map((e) => (
-        <li>
-          {e.description}:{" "}
-          <EditableField
-            value={e.yearlyAmount}
-            description="yearly"
-          ></EditableField>
-          , (m)$
-          {e.yearlyAmount / 12}
-          .00, (f)${e.yearlyAmount / 26}.00,, (w)${e.yearlyAmount / 56}.00,
-        </li>
-      ))}
-    </ul>
+    <NoStyleList>
+      {entries.map((expense) => {
+        console.log(expense);
+        console.log(expense.yearlyAmount.toString());
+        console.log(expense.expenseId);
+        return (
+          <li key={expense.expenseId}>
+            <div style={{ display: "flex" }}>
+              <EditableField
+                fieldKey={expense.yearlyAmount.toString()}
+                value={expense.expenseId}
+                description="uniqueId"
+                updateValue={updateUniqueId}
+              ></EditableField>
+              <EditableField
+                fieldKey={expense.expenseId}
+                value={expense.description}
+                description="description"
+                updateValue={updateExpenseDescription}
+              ></EditableField>
+              <EditableField
+                fieldKey={expense.expenseId}
+                value={expense.yearlyAmount.toFixed(2)}
+                description="yearly"
+                updateValue={updateExpenseValue}
+              ></EditableField>
+              <EditableField
+                fieldKey={expense.expenseId}
+                value={(expense.yearlyAmount / 12).toFixed(2)}
+                description="monthly"
+                updateValue={updateMonthlyExpenseValue}
+              ></EditableField>
+              <EditableField
+                fieldKey={expense.expenseId}
+                value={(expense.yearlyAmount / 26).toFixed(2)}
+                description="fortnightly"
+                updateValue={updateFortnightlyExpenseValue}
+              ></EditableField>
+              <EditableField
+                fieldKey={expense.expenseId}
+                value={(expense.yearlyAmount / 52).toFixed(2)}
+                description="weekly"
+                updateValue={updateWeeklyExpenseValue}
+              ></EditableField>
+            </div>
+          </li>
+        );
+      })}
+    </NoStyleList>
   );
 };
 
 interface Expense {
+  expenseId: string;
   yearlyAmount: number;
   description: string;
 }
-
 const Budget = () => {
-  let values: number[] = new Array();
-  values.push(2000);
-  values.push(5000);
-  values.push(7000);
+  const [entries, setEntries] = useState(() => {
+    const saved = localStorage.getItem("budgetExpenses");
+    const initialValue = saved && JSON.parse(saved);
+    return initialValue || "";
+  });
 
-  // const entries: number[] = [
-  const entries: Expense[] = [
-    { yearlyAmount: 10, description: "example" },
-    { yearlyAmount: 20, description: "example 2" },
-  ];
+  const entriesHandler = (updatedEntries: Expense[]) => {
+    console.log(`entries:`);
+    console.log(entries);
+    console.log(`updated: `);
+    console.log(updatedEntries);
+    setEntries(updatedEntries);
+  };
+
+  useEffect(() => {
+    console.log(`UPDATING LStorage:`);
+    console.log(entries);
+
+    localStorage.setItem("budgetExpenses", JSON.stringify(entries));
+  }, [entries]);
+
   return (
     <>
       <h1>Budget</h1>
-      <Entries value={entries} />
+      <Entries entries={[...entries]} updateEntries={entriesHandler} />
       <Total />
     </>
   );
