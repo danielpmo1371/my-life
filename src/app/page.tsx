c5c9346eda79bd56b3470f43b9348a464b98df74
Daniel Paiva <daniel.p@rushdigital.co.nz>
2023-11-20 21:16:24 +1300

    changed home page to include all links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,115 @@
-import Link from 'next/link'
-
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
-    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-      <h1 style={{ textAlign: 'center' }}>My Main Idea File</h1>
-      
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1 style={{ textAlign: "center" }}>My Main Idea File</h1>
+
       <section>
         <h2>Personal Dashboard</h2>
         <p>Overview of current focuses, events, deadlines.</p>
-        {/* Add more content or links here */}
       </section>
 
       <section>
         <h2>Links to Key Documents</h2>
         <ul>
-          <li><a href="#budget">Budget</a></li>
-          <li><a href="#weekly-schedule">Weekly Schedule</a></li>
-          <li><a href="#goals">Goals</a></li>
-          <li><a href="#goals">Projects</a></li>
-          <li><a href="#goals">To-Do Lists</a></li>
-          <li><a href="#goals">Important Contacts</a></li>
+          <li>
+            <a href="#budget">Budget</a>
+          </li>
+          <li>
+            <a href="#weekly-schedule">Weekly Schedule</a>
+          </li>
+          <li>
+            <a href="#goals">Goals</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#to-do-lists">To-Do Lists</a>
+          </li>
+          <li>
+            <a href="#important-contacts">Important Contacts</a>
+          </li>
         </ul>
       </section>
 
       <section>
         <h2>Health and Wellness</h2>
-        <p>Links to fitness plan, meal plans, medical records.</p>
         <ul>
-          <li><a href="#goals">Fitness Plan</a></li>
-          <li><a href="#goals">Meal Plans</a></li>
-          <li><a href="#goals">Medical Records</a></li>
+          <li>
+            <a href="#fitness-plan">Fitness Plan</a>
+          </li>
+          <li>
+            <a href="#meal-plans">Meal Plans</a>
+          </li>
+          <li>
+            <a href="#medical-records">Medical Records</a>
+          </li>
         </ul>
       </section>
 
       <section>
         <h2>Professional Development</h2>
-        <p>Links to career goals, skill development resources, resume.</p>
-        {/* Add more content or links here */}
+        <ul>
+          <li>
+            <a href="#career-goals">Career Goals</a>
+          </li>
+          <li>
+            <a href="#skill-development">Skill Development</a>
+          </li>
+          <li>
+            <a href="#resume-cv">Resume/CV</a>
+          </li>
+        </ul>
       </section>
 
-      {/* Add other sections as needed */}
+      <section>
+        <h2>Personal Development</h2>
+        <ul>
+          <li>
+            <a href="#reading-list">Reading List</a>
+          </li>
+          <li>
+            <a href="#learning-goals">Learning Goals</a>
+          </li>
+          <li>
+            <a href="#journal-reflections">Journal/Reflections</a>
+          </li>
+        </ul>
+      </section>
 
+      <section>
+        <h2>Financial Planning</h2>
+        <ul>
+          <li>
+            <a href="#investment-portfolio">Investment Portfolio</a>
+          </li>
+          <li>
+            <a href="#retirement-plan">Retirement Plan</a>
+          </li>
+          <li>
+            <a href="#debt-management">Debt Management</a>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Miscellaneous</h2>
+        <ul>
+          <li>
+            <a href="#travel-plans">Travel Plans</a>
+          </li>
+          <li>
+            <a href="#wish-list">Wish List</a>
+          </li>
+          <li>
+            <a href="#important-dates">Important Dates</a>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 };
 
 export default Home;
-
-
-
-// 1 Personal dashboards: Brief overview or dashboard of your current focus areas, upcoming events, or deadlines.
-// 2 Links to Key Documents:
-// Budget: Link to your personal budget document.
-// Weekly Schedule: Link to your calendar or weekly planner.
-// Goals: Link to a document outlining your short-term and long-term goals.
-// Projects: Link to a file detailing ongoing projects or side hustles.
-// To-Do Lists: Link to daily or weekly to-do lists.
-// Important Contacts: Link to a document with contact information for key people in your life (family, friends, colleagues, doctors, etc.).
-// 3 Health and Wellness:
-// Fitness Plan: Link to your workout schedule or fitness goals.
-// Meal Plans: Link to your weekly meal plans or dietary guidelines.
-// Medical Records: Link to a secure location where you keep your medical documents.
-// 4 Professional Development:
-// Career Goals: Link to a document outlining your career aspirations and steps to achieve them.
-// Skill Development: Link to resources or courses you're pursuing for professional growth.
-// Resume/CV: Quick access to your updated resume or CV.
-// 5 Personal Development:
-// Reading List: Link to your reading list or book summaries.
-// Learning Goals: Link to a list of topics or skills you want to learn.
-// Journal/Reflections: Link to your personal journal or reflection logs.
-// 6 Financial Planning:
-// Investment Portfolio: Link to a summary of your investments.
-// Retirement Plan: Link to details about your retirement planning.
-// Debt Management: Link to a plan or tracker for managing and paying off debts.
-// 7 Miscellaneous:
-// Travel Plans: Link to upcoming travel itineraries or wish lists.
-// Wish List: Link to a list of items or experiences you wish to acquire.
-// Important Dates: Link to a list of important dates like anniversaries, birthdays, etc.
-  
