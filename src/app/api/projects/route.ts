1b7bafe52880da42c08926b043eeb182fb5d86a1
Daniel Paiva <daniel.p@rushdigital.co.nz>
2023-12-16 21:45:38 +1300

    filter projects by owner

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { getSession } from "@auth0/nextjs-auth0";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
@@ -6,12 +7,18 @@ const prisma = new PrismaClient({
 });
 
 export async function GET(_req: any, res: { json: (arg0: any) => void }) {
+  const session = await getSession();
+  const user = session?.user;
+
+  if (!user) return;
+
   try {
     await prisma.$connect();
-    const projects = await prisma.projects.findMany();
+    const projects = await prisma.projects.findMany({
+      where: { ownerEmail: { equals: user.email } },
+    });
 
     prisma.$disconnect();
-    // return projects;
     return Response.json(projects);
   } catch (e) {
     console.error(e);
