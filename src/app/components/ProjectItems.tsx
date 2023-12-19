87019e5b8205d9f788c0386d87f132be8511b5cf
Daniel Paiva <daniel.p@rushdigital.co.nz>
2023-12-19 16:33:05 +1300

    applied cst to project items

--- src/app/components/ProjectItems.tsx
+++ src/app/components/ProjectItems.tsx
@@ -3,6 +3,7 @@ import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import CustomButton from "@/components/CustomListItem";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { getCrudFor } from "@/next_cst/crudClient";
 
 type Project = {
   id?: string;
@@ -11,48 +12,8 @@ type Project = {
   ownerEmail: string;
 };
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-const getProjectsUrl = `${BASE_URL}api/projects`;
-
-async function getData(setData: Dispatch<SetStateAction<Project[]>>) {
-  return await fetch(getProjectsUrl).then(async (response) => {
-    return await response.json().then((data) => {
-      setData(data);
-    });
-  });
-}
-
-async function saveDataAndRefresh(
-  newProject: Project,
-  setData: Dispatch<SetStateAction<Project[]>>
-) {
-  return await fetch(getProjectsUrl, {
-    method: "POST",
-    body: JSON.stringify(newProject),
-  }).then(async (response) => {
-    return await response.json().then((data) => {
-      console.log(JSON.stringify(data));
-      setData(data);
-      return data;
-    });
-  });
-}
-
-async function deleteAndRefresh(
-  projToDelete: Project,
-  setData: Dispatch<SetStateAction<Project[]>>
-) {
-  return await fetch(getProjectsUrl, {
-    method: "DELETE",
-    body: JSON.stringify(projToDelete),
-  }).then(async (response) => {
-    return await response.json().then((data) => {
-      console.log(`updated from del resp: ${JSON.stringify(data)}`);
-      setData(data);
-      return data;
-    });
-  });
-}
+const { getData, saveDataAndRefresh, deleteAndRefresh } =
+  getCrudFor<Project>("projects");
 
 export default function ProjectItems() {
   const { user } = useUser();
@@ -70,7 +31,7 @@ export default function ProjectItems() {
 
   return (
     <div style={{ flex: 1 }}>
-      <h4>Project items</h4>
+      <h4>Projects</h4>
       <ul>
         {data?.map((t) => (
           <li
