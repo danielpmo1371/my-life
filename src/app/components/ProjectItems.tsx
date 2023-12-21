96f57f408a3fb42761be1533fb3f718795aae033
Daniel Paiva <daniel.p@rushdigital.co.nz>
2023-12-22 07:22:57 +1300

    added project description

--- src/app/components/ProjectItems.tsx
+++ src/app/components/ProjectItems.tsx
@@ -21,6 +21,22 @@ export default function ProjectItems() {
     user && (
       <div style={{ flex: 1 }}>
         <h4>Projects</h4>
+        <h6 className="text-with-hidden-explanation">
+          Projects are structured endeavors that...
+          <span className="hide">
+            involve multiple tasks, requiring coordination, planning, and a
+            dedicated timeline for completion. Unlike abstract mind concepts
+            (AMCs) or to-do items, projects involve various steps and
+            components. They are defined by specific objectives, a start and end
+            date, and often require resources such as time, skills, and
+            resources. Examples of projects could include organizing a trip,
+            developing a new business strategy, or renovating a home. Each
+            project is a journey towards a defined goal, involving
+            problem-solving, decision-making, and often teamwork. They provide a
+            sense of accomplishment upon completion and contribute significantly
+            to personal or professional growth.
+          </span>
+        </h6>
         <PillListOfItems<Project>
           crudClient={crudClient}
           user={user}
