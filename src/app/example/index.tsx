f2e413dc710643e286b201abf2ae936009a9fe81
Daniel Paiva <daniel.p@rushdigital.co.nz>
2023-12-22 13:11:49 +1300

    removed example stale file

--- src/app/example/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Project } from '../../types/project';
-import ProjectCard from '@/components/ProjectCard';
-// import { ProjectCard } from './card';
-
-export default function Projects() {
-return <h2>Projects</h2>;   
-};
